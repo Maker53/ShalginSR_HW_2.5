26de81c092c8d8974a0b3043fea8f4a972702482
Maker53 <s.shalgin@gmail.com>
2021-10-29 21:01:46 +0300

    Create storyboards and create quiz model

--- PersonalQuiz/ViewControllers/ViewController.swift
+++ PersonalQuiz/ViewControllers/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let questions = Question.getQuestions()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
